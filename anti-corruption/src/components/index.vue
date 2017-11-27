feafe45b74938e0cb3bde5290d015cabc8c014c2
aifly <aifly_xc@163.com>
2017-11-27 18:14:00 +0800

    a

---
+++ anti-corruption/src/components/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div>index app</div>
+		<div>{{name}}</div>
+	</div>
+
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				name:'fly'
+			}
+		}
+	}
+</script>
