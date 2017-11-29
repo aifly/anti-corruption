2284370b0f400da65c58e71aa1288e6b8e8d70ab
aifly <aifly_xc@163.com>
2017-11-29 18:26:32 +0800

    add

--- anti-corruption/src/components/index.vue
+++ anti-corruption/src/components/index.vue
@@ -1,16 +1,59 @@
 <template>
-	<div>
-		<div>index app</div>
-		<div>{{name}}</div>
+	<div class="zmiti-index-main-ui lt-full" @touchmove='touchmove($event)' @touchstart='touchstart($event)' @touchend='touchend($event)' >
+		<div ref='aa' class="zmiti-index-bottom-img-C" :style="{transform:'translate(0,'+transX+'px)'}">
+			<img src="../assets/images/bg.png">
+			<img src="../assets/images/bg.png">
+		</div>
 	</div>
-
 </template>
 
 <script>
+	import './css/index.css';
 	export default{
 		data(){
 			return {
-				name:'fly'
+				transX:0
+			}
+		},
+		created(){
+
+			setTimeout(()=>{
+				var height = this.$refs.aa.offsetHeight;
+				var x =-1;
+				this.speed = x;
+				setInterval(()=>{
+					this.transX+=this.speed;
+					if(this.transX<=-height/2){
+						this.transX = 0;
+					}
+				},20)
+			},100)
+		},
+		methods:{
+			touchstart(e){
+				this.startY = e.changedTouches[0].pageY;
+
+				this.startTime = new Date().getTime();
+				this.initSpeedTimer && clearTimeout(this.initSpeedTimer)
+			},
+			touchmove(e){
+				e.preventDefault();
+			//	console.log(e);
+			},
+			touchend(e){
+				this.endY = e.changedTouches[0].pageY;
+				var disY = this.startY -this.endY;
+				if(disY>50){
+					this.endTime = new Date().getTime()
+
+					this.speed = - Math.min((this.endTime - this.startTime)/20,10);
+
+					console.log(this.speed )
+					//this.startTime = this.endTime;
+					this.initSpeedTimer = setTimeout(()=>{
+						this.speed = -1;
+					},100)
+				}
 			}
 		}
 	}
--- anti-corruption/src/router/index.js
+++ anti-corruption/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import index from '@/components/index';
 
 Vue.use(Router)
@@ -8,10 +7,6 @@ Vue.use(Router)
 export default new Router({
 	routes: [{
 		path: '/',
-		name: 'HelloWorld',
-		component: HelloWorld
-	}, {
-		path: '/index',
 		name: 'index',
 		component: index
 	}]
